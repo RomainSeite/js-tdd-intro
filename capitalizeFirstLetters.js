2280964476f3eb9116f7c6eeee3f40b2567e1ba2
Romain SEITE <romain-seite_student2021@wilder.school>
2021-03-22 16:27:17 +0100

    Wrote Tests

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,1 +1,29 @@
+    une chaîne avec plusieurs mots
 
+    une chaîne avec un seul mot (voire une seule lettre)
+
+    une chaîne vide
+    
+    // capitalizeFirst.js
+
+const assert = require('assert');
+
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+
+// Check that it works with several words
+
+assert.strictEqual(capitalizeFirst("i am learning TDD"), "I Am Learning TDD)
+
+// Check that it works with one word
+
+assert.strictEqual(capitalizeFirst("javascript, 'Javascript');
+
+// Check that it works with one letter
+
+assert.strictEqual(capitalizeFirst("m", 'M');
+
+// Check that it works with an empty string
+
+assert.strictEqual(capitalizeFirst(" "), " ");
