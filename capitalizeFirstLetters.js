d1aeb971bbc8148f4aed5393a24a7afbf904f0dc
Romainseite <seite.romain@gmail.com>
2021-03-23 11:06:51 +0100

    refactored capitalizeFirstLetters

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,32 +1,32 @@
-    // capitalizeFirst.js
+    // capitalizeFirstLetters.js
 
 const assert = require('assert');
 
 
 // WRITE THE ACTUAL FUNCTION HERE
-function capitalizeFirst(string) {
+function capitalizeFirstLetters(string) {
   let words = new Array();
-  string.split` `.forEach(word =>
-    word.length > 0?
-    words.push(word[0].toUpperCase() + word.slice(1))
+  string.split` `.forEach(word => word.length > 0
+  	?words.push(word[0].toUpperCase() + word.slice(1))
     :words.push(''))
-  return words.join(' ');
+  return words.join` `;
 }
 
 
 // Check that it works with several words
-
 assert.strictEqual(capitalizeFirst("i am learning TDD"), "I Am Learning TDD")
 
 // Check that it works with one word
-
 assert.strictEqual(capitalizeFirst("javascript"), 'Javascript');
 
 // Check that it works with one letter
-
 assert.strictEqual(capitalizeFirst("m"), 'M');
 
 // Check that it works with an empty string
+assert.strictEqual(capitalizeFirst(""), "");
 
+// Check that it works with a whitespace string
 assert.strictEqual(capitalizeFirst(" "), " ");
 
+// Check that it works with an integer
+assert.strictEqual(capitalizeFirst(1), 1);
